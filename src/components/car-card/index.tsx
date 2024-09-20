cb143edc92af98433560d30f56de49828d848891
noyonalways <noyonrahman2003@gmail.com>
2024-09-20 22:40:59 +0600

    feat: featured cars section added

---
+++ src/components/car-card/index.tsx
@@ -0,0 +1,43 @@
+import { User } from "lucide-react";
+import { TbManualGearbox } from "react-icons/tb";
+import { Button } from "../ui/button";
+
+interface IProps {}
+
+const CarCard: React.FC<IProps> = () => {
+  return (
+    <div className="border p-4 rounded-xl relative">
+      <h2 className="text-lg font-semibold">Range Rover Evoque</h2>
+      <figure className="p-4 mb-2">
+        <img
+          className="w-full"
+          src="https://i.ibb.co.com/kqpbXz8/white-sample-car.png"
+          alt="car-image"
+        />
+      </figure>
+      <div className="flex justify-between items-center mb-2">
+        <div className="flex items-center space-x-10">
+          <div className="flex space-x-2 items-center">
+            <User className="text-primary" size={24} />
+            <p>4</p>
+          </div>
+          <div className="flex space-x-2 items-center">
+            <TbManualGearbox className="text-primary" size={24} />
+            <p>4</p>
+          </div>
+        </div>
+        <div className="font-medium">
+          <span>৳ 450</span>
+          <span>/hr</span>
+        </div>
+      </div>
+      <div className="absolute left-0 top-0 w-full bg-primary/90 h-full rounded-xl flex items-center justify-center opacity-0 hover:opacity-100 duration-200 transition-all cursor-pointer">
+        <Button className="border dark:hover:border-primary border-white bg-transparent hover:bg-background hover:text-primary active:scale-95 duration-200">
+          View Details
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
--- src/pages/home/featured-cars/index.tsx
+++ src/pages/home/featured-cars/index.tsx
@@ -1,10 +1,43 @@
+import CarCard from "@/components/car-card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
 interface IProps {}
 
 const FeaturedCars: React.FC<IProps> = () => {
   return (
-    <section className="pb-20">
+    <section className="py-20">
       <div className="container">
-        <h1>Featured Cars</h1>
+        <div className="text-center space-y-2 mb-16">
+          <h3 className="text-primary font-semibold tracking-[.15rem]">
+            Featured Cars
+          </h3>
+          <h1 className="font-bold text-3xl">Our Featured Cars</h1>
+        </div>
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+          opts={{
+            align: "start",
+          }}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="basis-full lg:basis-1/4">
+                <CarCard />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious />
+          <CarouselNext /> */}
+        </Carousel>
       </div>
     </section>
   );
